e9bb9a4e7af9ffee72b9821672ab04de24b4966e
Umer Azhar <uazhar@rentsync.com>
2025-08-01 19:34:40 -0400

    placeholder text

--- src/app/components/TaskForm.tsx
+++ src/app/components/TaskForm.tsx
@@ -47,8 +47,9 @@ const TaskForm: FC<TTaskFormProps> = ({
       <div>
         <label className="block mb-1 font-semibold">Title</label>
         <input
-          className="w-full px-4 py-2 rounded bg-gray-800 border border-gray-600 focus:outline-none focus:ring-2 focus:ring-blue-500"
+          className="w-full px-4 py-2 rounded bg-input-background border border-background placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500"
           value={title}
+          placeholder='Ex. Brush your teeth'
           onChange={(e) => setTitle(e.target.value)}
           required
         />
